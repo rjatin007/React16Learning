a85c8e5f763a4e01afaccbacf00ae3a0bd5dea55
rjatin007 <30732342+rjatin007@users.noreply.github.com>
2017-12-02 00:56:59 +0530

    Create Adding_pseudoSelectors_CSS_Modules.js

---
+++ Adding_pseudoSelectors_CSS_Modules.js
@@ -0,0 +1,113 @@
+//In App.js
+import React, { Component } from 'react';
+import Styles from './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+state={
+  persons:[
+    { id: 1, name : 'Jatin' , age : 26 },
+    { id: 2, name : 'Riship' , age : 26 },
+    { id: 3, name : 'Amit' , age : 28 }
+  ],
+  showPersons:false
+}
+
+onChangeHandler=(event,id)=>{
+  let val = event.target.value
+  const personIndex=this.state.persons.findIndex(p=>(p.id===id))
+  const person={...this.state.persons[personIndex]};
+  person.name = val;
+  const persons=[...this.state.persons];
+  persons[personIndex]=person;
+  this.setState({persons});
+}
+deletePerson=(index)=>{
+//const persons=this.state.persons.slice();// its a pointer reference
+const persons = [...this.state.persons];
+persons.splice(index,1); 
+this.setState({persons});
+}
+onToggle=()=>{
+  const show = this.state.showPersons;
+  this.setState({
+    showPersons : !show
+  });
+}
+  render() {
+    const { persons, showPersons } = this.state;
+    let showCase = null;
+    let buttonClass ='';
+    if(showPersons){
+     showCase  =  (
+          persons.map((person,index)=>(
+            <Person key={person.id} name={person.name}
+              click={()=>this.deletePerson(index)}
+              age = {person.age}
+              changed={(event)=> this.onChangeHandler(event,person.id)}/>
+          )
+        )
+      )
+      buttonClass= Styles.red;
+     
+    }
+    // let classes=['red','bold'].join(' ');// red bold
+    const classes = [];
+    if(persons.length<=2){
+      classes.push(Styles.red);
+    }
+    if(persons.length<=1){
+      classes.push(Styles.bold);
+    }
+    return (
+    
+        <div className={Styles.App}>
+          <p className ={classes.join(' ')}>Hello</p>
+            {showCase}
+            <button 
+            className={buttonClass}
+            onClick={this.onToggle}>
+            Render Conditionally
+          </button> 
+        </div>
+      
+    );
+  }
+}
+
+export default App;
+
+
+// In App.css
+.App {
+  text-align: center;
+}
+
+.red{
+  color: red;
+}
+.bold{
+  font-weight:bold;
+}
+.App button {
+
+    background-color : green;
+    color : white;
+    border-radius: 10px;
+    font : inherit;
+    border : 1px solid blue;
+    padding : 8px;
+    cursor: pointer;
+    margin-top :50px
+}
+.App button:hover {
+  background-color: lightgreen;
+  color: black;
+}
+.App button.red{
+  background-color: red;
+}
+.App button.red:hover{
+  background-color: salmon;
+  color: black;
+}
